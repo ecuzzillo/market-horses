7554812ab3b25ee1d0bcbaf9b6a9c1e21d4b00f5
Chris Atwood <coachchris289@gmail.com>
2020-01-18 16:57:44 -0800

    Chris main add

---
+++ Assets/HelloElliotScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloElliotScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+          Debug.log("Hello darkness, my old friend")
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
