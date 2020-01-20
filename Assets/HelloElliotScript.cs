5ff924ea7d3ec7b5daf651946e57bf7ab3c280e1
Chris Atwood <coachchris289@gmail.com>
2020-01-19 22:01:18 -0800

    make modal appear and disappear

--- Assets/HelloElliotScript.cs
+++ Assets/HelloElliotScript.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class HelloElliotScript : MonoBehaviour
 {
+    public GameObject OpenModal;
+
+
     public void MyMessage()
     {
+        //gameObject.SetActive(false);
+        if (gameObject.active)
+            gameObject.SetActive(false);
+        else
+            gameObject.SetActive(true);
+    
         Debug.Log("hello darkness, my old friend");
     }
 
