bfc114f7bfa87b72a249e53bb0aa18c4d684dfa8
Elliot Cuzzillo <elliotc@unity3d.com>
2020-01-19 20:57:57 -0800

    everybody can sign up

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
+    NetworkIdentity id;
+    bool added = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,22 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!added && !isServer && ClientScene.localPlayers.Count > 0)
+        {
+            added = true;
+            id = GetComponent<NetworkIdentity>();
+            if (id.hasAuthority)
+            {
+                Debug.Log("trying to add player info for " + id.isLocalPlayer);
+                CmdAddPlayerInfo();
+            }
+        }
+    }
+
+    [Command]
+    void CmdAddPlayerInfo()
+    {
+        Debug.Log("trying to add player info for " + id);
+        bank.Instance.AddPlayerInfo(id);
     }
 }
