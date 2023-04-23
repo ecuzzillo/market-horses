228dc09a3ae1d11cf5f7c5eeff9a92a4dfb6161a
Robert Stratton <bobby@middesk.com>
2023-04-22 17:47:17 -0700

    Add some UI Toolkit stuff

---
+++ market-horses/Assets/Scripts/SimpleRuntimeUI.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SimpleRuntimeUI : MonoBehaviour
+{
+    private Button _button;
+    private Toggle _toggle;
+
+    private int _clickCount;
+
+    //Add logic that interacts with the UI controls in the `OnEnable` methods
+    private void OnEnable()
+    {
+        // The UXML is already instantiated by the UIDocument component
+        var uiDocument = GetComponent<UIDocument>();
+
+        _button = uiDocument.rootVisualElement.Q(className: "cows-trade-button") as Button;
+        _button.RegisterCallback<ClickEvent>(CowsTradeButtonHandler);
+    }
+
+    private void OnDisable()
+    {
+        _button.UnregisterCallback<ClickEvent>(CowsTradeButtonHandler);
+    }
+
+    public void CowsTradeButtonHandler(ClickEvent evt)
+    {
+        var uiDocument = GetComponent<UIDocument>();
+        var fundManagers = uiDocument.rootVisualElement.Q(className: "fund-managers");
+        var displayStyle = fundManagers.style.display;
+        if (displayStyle == null || displayStyle == DisplayStyle.Flex)
+        {
+            fundManagers.style.display = DisplayStyle.None;
+        }
+        else
+        {
+            fundManagers.style.display = DisplayStyle.Flex;
+        }
+    }
+}
