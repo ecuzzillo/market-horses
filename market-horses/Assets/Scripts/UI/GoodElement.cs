b3ff04dd7a05c91e9cd23f82ee030a0066de8aa7
Robert Stratton <bobby@middesk.com>
2023-04-22 21:54:05 -0700

    Get UI somewhat working

---
+++ market-horses/Assets/Scripts/UI/GoodElement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GoodElement : VisualElement
+{
+    // Expose the custom control to UXML and UI Builder.
+    public new class UxmlFactory : UxmlFactory<GoodElement> { }
+
+    public GoodType goodType;
+    // TODO: Do we need to do this every time or can we cache it?
+    public Label goodName => this.Q<Label>("good-name");
+    public Button tradeButton => this.Q<Button>("trade-button");
+
+    // Custom controls need a default constructor. This default constructor 
+    // calls the other constructor in this class.
+    public GoodElement() { }
+
+    public GoodElement(GoodType goodType)
+    {
+        this.goodType = goodType;
+        var asset = Resources.Load<VisualTreeAsset>("GoodElement");
+        asset.CloneTree(this);
+
+        goodName.text = goodType.ToString();
+        tradeButton.RegisterCallback<ClickEvent>(OnTradeClick);
+    }
+
+    public void OnTradeClick(ClickEvent evt)
+    {
+        UIManager.Instance.ShowTradeView(goodType);
+    }
+}
---
+++ market-horses/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIManager : MonoBehaviour
+{
+    public UIDocument document;
+    public VisualElement tickerSection => document.rootVisualElement.Q("ticker-section");
+    public VisualElement managersSection => document.rootVisualElement.Q("managers-section");
+    public VisualElement goodsSection => document.rootVisualElement.Q("goods-section");
+    public VisualElement tradeSection => document.rootVisualElement.Q("trade-section");
+
+    public GoodType goodToTrade;
+
+    public static UIManager Instance;
+
+    public void Start()
+    {
+        Instance = this;
+
+        document = GetComponent<UIDocument>();
+
+        var asset = Resources.Load<VisualTreeAsset>("Main");
+        asset.CloneTree(document.rootVisualElement);
+
+        foreach (var goodType in (GoodType[]) Enum.GetValues(typeof(GoodType)))
+        {
+            var goodElement = new GoodElement(goodType);
+            document.rootVisualElement.Q("goods-list").Add(goodElement);
+        }
+
+        tradeSection.style.display = DisplayStyle.None;
+
+        document.rootVisualElement.Q("buy-button").RegisterCallback<ClickEvent>(OnTradeBuyClick);
+        document.rootVisualElement.Q("sell-button").RegisterCallback<ClickEvent>(OnTradeSellClick);
+    }
+
+    public void ShowTradeView(GoodType goodType)
+    {
+        goodToTrade = goodType;
+        managersSection.style.display = DisplayStyle.None;
+        goodsSection.style.display = DisplayStyle.None;
+        tradeSection.style.display = DisplayStyle.Flex;
+    }
+
+    public void OnTradeBuyClick(ClickEvent evt)
+    {
+        ShowMainView();
+    }
+
+    public void OnTradeSellClick(ClickEvent evt)
+    {
+        ShowMainView();
+    }
+
+    public void ShowMainView()
+    {
+        managersSection.style.display = DisplayStyle.Flex;
+        goodsSection.style.display = DisplayStyle.Flex;
+        tradeSection.style.display = DisplayStyle.None;
+    }
+}
